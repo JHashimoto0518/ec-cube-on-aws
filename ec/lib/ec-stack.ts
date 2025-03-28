9a0c67d8d69ff279fb4ca16b5b161f85965c93c0
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2025-03-29 08:07:12 +0900

    add: web-ec2 wih cdk

---
+++ ec/lib/ec-stack.ts
@@ -0,0 +1,59 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+export class EcStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // vpc
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      vpcName: 'vpc',
+      ipAddresses: ec2.IpAddresses.cidr('172.16.0.0/16'),
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+        }
+      ],
+      // remove all rules from default security group
+      // See: https://docs.aws.amazon.com/config/latest/developerguide/vpc-default-security-group-closed.html
+      restrictDefaultSecurityGroup: true,
+    });
+
+    // security group
+    const webSg = new ec2.SecurityGroup(this, 'WebSg', {
+      vpc,
+      allowAllOutbound: true,
+      description: 'security group for web ec2'
+    })
+    webSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http traffic from anywhere')
+
+    // ec2
+    const webIns = new ec2.Instance(this, 'Web', {
+      instanceName: 'ec-lab-ec2-web',
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      vpc,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      }),
+      securityGroup: webSg,
+      blockDevices: [
+        {
+          deviceName: '/dev/xvda',
+          volume: ec2.BlockDeviceVolume.ebs(8, {
+            encrypted: true
+          }),
+        },
+      ],
+    })
+  }
+}
